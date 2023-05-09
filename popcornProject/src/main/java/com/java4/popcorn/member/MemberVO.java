abac7cf15c8d09f9f258a9036a34c0249d2853c8
Lee si hyeon <this.seeya@gmail.com>
2023-05-09 15:39:52 +0900

    memberVO 생성

---
+++ popcornProject/src/main/java/com/java4/popcorn/member/MemberVO.java
@@ -0,0 +1,76 @@
+package com.java4.popcorn.member;
+
+public class MemberVO {
+	private String member_id;
+	private String member_pw;
+	private String member_name;
+	private String member_knickname;
+	private String member_tel;
+	private String member_email;
+	private int member_level;
+	private int member_fav;
+	private int member_view;
+	private String member_img;
+	
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_pw() {
+		return member_pw;
+	}
+	public void setMember_pw(String member_pw) {
+		this.member_pw = member_pw;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getMember_knickname() {
+		return member_knickname;
+	}
+	public void setMember_knickname(String member_knickname) {
+		this.member_knickname = member_knickname;
+	}
+	public String getMember_tel() {
+		return member_tel;
+	}
+	public void setMember_tel(String member_tel) {
+		this.member_tel = member_tel;
+	}
+	public String getMember_email() {
+		return member_email;
+	}
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+	public int getMember_level() {
+		return member_level;
+	}
+	public void setMember_level(int member_level) {
+		this.member_level = member_level;
+	}
+	public int getMember_fav() {
+		return member_fav;
+	}
+	public void setMember_fav(int member_fav) {
+		this.member_fav = member_fav;
+	}
+	public int getMember_view() {
+		return member_view;
+	}
+	public void setMember_view(int member_view) {
+		this.member_view = member_view;
+	}
+	public String getMember_img() {
+		return member_img;
+	}
+	public void setMember_img(String member_img) {
+		this.member_img = member_img;
+	}
+	
+}
